2387bcead9874749f3c6c174f1ee2a042cdc49cf
Daneil Blyth <rdblyth@gmail.com>
2016-02-03 20:03:31 -0500

    Formatted code

--- actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -1,6 +1,6 @@
 /**
- * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
- */
+  * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
+  */
 package actorbintree
 
 import akka.actor._
@@ -12,7 +12,9 @@ object BinaryTreeSet {
 
   trait Operation {
     def requester: ActorRef
+
     def id: Int
+
     def elem: Int
   }
 
@@ -21,33 +23,33 @@ object BinaryTreeSet {
   }
 
   /**
-   * Request with identifier `id` to insert an element `elem` into the tree.
-   * The actor at reference `requester` should be notified when this operation
-   * is completed.
-   */
+    * Request with identifier `id` to insert an element `elem` into the tree.
+    * The actor at reference `requester` should be notified when this operation
+    * is completed.
+    */
   case class Insert(requester: ActorRef, id: Int, elem: Int) extends Operation
 
   /**
-   * Request with identifier `id` to check whether an element `elem` is present
-   * in the tree. The actor at reference `requester` should be notified when
-   * this operation is completed.
-   */
+    * Request with identifier `id` to check whether an element `elem` is present
+    * in the tree. The actor at reference `requester` should be notified when
+    * this operation is completed.
+    */
   case class Contains(requester: ActorRef, id: Int, elem: Int) extends Operation
 
   /**
-   * Request with identifier `id` to remove the element `elem` from the tree.
-   * The actor at reference `requester` should be notified when this operation
-   * is completed.
-   */
+    * Request with identifier `id` to remove the element `elem` from the tree.
+    * The actor at reference `requester` should be notified when this operation
+    * is completed.
+    */
   case class Remove(requester: ActorRef, id: Int, elem: Int) extends Operation
 
-  /** Request to perform garbage collection*/
+  /** Request to perform garbage collection */
   case object GC
 
   /**
-   * Holds the answer to the Contains request with identifier `id`.
-   * `result` is true if and only if the element is present in the tree.
-   */
+    * Holds the answer to the Contains request with identifier `id`.
+    * `result` is true if and only if the element is present in the tree.
+    */
   case class ContainsResult(id: Int, result: Boolean) extends OperationReply
 
   /** Message to signal successful completion of an insert or remove operation. */
@@ -56,6 +58,7 @@ object BinaryTreeSet {
 }
 
 class BinaryTreeSet extends Actor {
+
   import BinaryTreeSet._
   import BinaryTreeNode._
 
@@ -96,10 +99,10 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /**
-   * Handles messages while garbage collection is performed.
-   * `newRoot` is the root of the new binary tree where we want to copy
-   * all non-removed elements into.
-   */
+    * Handles messages while garbage collection is performed.
+    * `newRoot` is the root of the new binary tree where we want to copy
+    * all non-removed elements into.
+    */
   def garbageCollecting(newRoot: ActorRef): Receive = {
     case Insert(requestor, id, elem) => {
       pendingQueue = pendingQueue :+ Insert(requestor, id, elem)
@@ -121,18 +124,22 @@ class BinaryTreeSet extends Actor {
 }
 
 object BinaryTreeNode {
+
   trait Position
 
   case object Left extends Position
+
   case object Right extends Position
 
   case class CopyTo(treeNode: ActorRef)
+
   case object CopyFinished
 
   def props(elem: Int, initiallyRemoved: Boolean) = Props(classOf[BinaryTreeNode], elem, initiallyRemoved)
 }
 
 class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
+
   import BinaryTreeNode._
   import BinaryTreeSet._
 
@@ -222,9 +229,9 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
 
   // optional
   /**
-   *
-   * `expected` is the set of ActorRefs whose replies we are waiting for,
-   * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
-   */
+    *
+    * `expected` is the set of ActorRefs whose replies we are waiting for,
+    * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
+    */
   def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
 }
